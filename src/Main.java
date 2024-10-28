51b8cae23a847d76b5010ab8fbc35bbb7a40b985
Mohamed Atef <proofesser@gmail.com>
2024-10-28 19:41:38 +0300

    started using mathmatical operators with Scanner object

---
+++ src/Main.java
@@ -0,0 +1,162 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in); // object
+        System.out.println("Enter your username!");
+        String name = reader.nextLine();
+        System.out.println("User name is: " + name);
+
+        System.out.println("Where do you study?");
+        String locationOfStudy = reader.nextLine();
+        System.out.println("Student location is: " + locationOfStudy);
+
+        System.out.println("What is this Digital Egypt that you are talking about! I have never heard about it!");
+        String depiQAnswer = reader.nextLine();
+        System.out.println("DEPI is: "+ depiQAnswer);
+
+        System.out.println("How long is this initiative? Provide answer in months!");
+        int depiLength = reader.nextInt();
+        System.out.println("DEPI length is " + depiLength + " months");
+
+        // 1.() ^
+        // 2. * /
+        // 3. + -
+
+//
+//        int x = 11;
+//        int y = 5;
+//        int z = 2;
+
+//        int result = (x - y) * z;
+
+//        int result = (10 + ( 5 - 3)) * 4 / 2;
+
+//        int result = (((25 - 5) * 4) / (2 - 10)) + 4;
+//        System.out.println(result);
+
+
+
+
+
+// mathematical operators
+        // addition +
+        // subtraction -
+        // multiplication
+        // division /
+        // %
+
+//        int x = 6;
+//        int y = 9;
+//
+//        // addition
+//        int result = x + y;
+//        System.out.println("Addition value: for "+ x + " + " + y + " = " + result);
+//
+//        // subtraction
+//        int subtraction = x - y;
+//        System.out.println("Subtraction value for " + x + " - " + y + " = " + subtraction);
+//
+//        // multiplication
+//
+//        int multiplication = x * y;
+//        System.out.println("Multiplication value for " + x + " * " + y + " = " + multiplication);
+//
+//        // division
+//        int division = x / y;
+//        System.out.println("Division value for " + x + " / " + y + " = " + division);
+//
+//        // reminder
+//        int reminder = y % x;
+//        System.out.println("Reminder value for " + x + " % " + y + " = " + reminder);
+
+
+        // variable declaration / definition
+//        int  length = 0;
+////        int anotherLength = length++; // post-increment
+//        int anotherLength = ++length; // pre-increment
+//
+//        System.out.println("Length : " + anotherLength);
+
+
+
+
+
+//        double width = 0;
+
+
+//        length ++; // increment length = length + 1; post increment
+//        ++length; // pre-increment
+//        int anotherLength = length;
+
+
+
+//        length --; // decrement length = length -1;
+
+
+
+
+        /*
+        8 data types => primitives => stack
+         */
+
+        // heap
+//
+//        String firstName = "Mohamed", lastName = "Atef";
+//
+//        System.out.println(firstName + " " + lastName);
+//
+//        firstName += "asasd";
+//
+//
+//
+//
+//
+//        // 8 data types - primitives  -- > stack
+//        boolean isSunSet = true; // camel case naming
+//        boolean isLeaving = true;
+
+
+
+//        char emailChar = '@';
+//        System.out.println("Char is : " + emailChar );
+//
+//        double salary = 15000;
+//        System.out.println("salary = " + salary);
+//
+//        float totalPrice = 100.5f;
+//        System.out.println("Total Price: " + totalPrice);
+//
+//
+//        long startTime = System.currentTimeMillis();
+//        System.out.println("start time: " + startTime);
+//
+//        short myShort = 500;
+//        System.out.println("my Short value:" + myShort);
+//
+//        byte tones = 120;
+//        System.out.println("Tones = " + tones);
+
+//        int x; // variable declaration - definition
+//        int y;
+//
+//        // initialization
+//        x = 5;
+//        y = 3;
+//
+//
+//        int age = 30;
+//        int length = 174;
+//
+//        System.out.println("My age is: "+ age + ", and my length is: "+ length);
+
+
+
+
+    }
+
+}
