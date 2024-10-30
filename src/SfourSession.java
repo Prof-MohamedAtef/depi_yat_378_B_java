d66f43f838a600b6406663c1c81a57dd8a554e69
Mohamed Atef <proofesser@gmail.com>
2024-10-30 19:44:05 +0300

    s-four session - lecture 2 - primitives

---
+++ src/SfourSession.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class SfourSession {
+    public static void main(String[] args) {
+
+//          // 8 primitives are saved into stack and considered as mutable data types
+//        boolean isDone = true;
+//        System.out.println(isDone);
+
+//        // stack - primitive - mutable
+//        char myCharacter = 's';
+//        System.out.println("my character is: " + myCharacter);
+//        // stack
+//        double myDoubleData = 653.3;
+//        System.out.println(myDoubleData);
+
+        // saved into stack as it's mutable / primitive data type
+//        float myFloatNum = 9.2f;
+//        System.out.println(myFloatNum);
+
+        // stack - primitive / mutable data type
+//        long myLongDataType = 99L;
+//        System.out.println("myLongDataType: " + myLongDataType);
+
+//        //stack --> primitive and mutable data type
+//        short myShortVariable = 3;
+//        System.out.println("myShortVariable: " +myShortVariable);
+
+        // stack -> mutable / primitive data type
+//        byte myByteVariable = 8; // definition - declaration => initialization
+//        System.out.println("Byte variable = " + myByteVariable);
+
+
+        // int is saved into stack as it's primitive / mutable data type - stack allocation
+        // int data type
+//        int width; // declaration - definition
+//        int height;
+//
+//        width = 8; // initial value
+//        height = 4;
+//
+//        // multiply width by height
+//
+//        System.out.println("Multiplication of Width = " + width + ", and height = " + height + " = " + (width*height));
+    }
+}
