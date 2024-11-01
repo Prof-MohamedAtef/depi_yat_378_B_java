3a193beb994e6ef9020b87813609094ae83e2fad
Mohamed Atef <proofesser@gmail.com>
2024-11-01 19:41:51 +0200

    switch statement

--- src/yatzaqziq/lecturethree/LectureThree.java
+++ src/yatzaqziq/lecturethree/LectureThree.java
@@ -2,19 +2,43 @@ package yatzaqziq.lecturethree;
 
 public class LectureThree {
     public static void main(String[] args) {
+
+        // control statements
+//        switch statement
         int weight, height;
-        weight = 80;
+        weight = 10;
         height = 180;
-        if (weight == height){
-            System.out.println("Weight is equal");
-        }else if (weight > height){
-            System.out.println("Weight is greater than height");
-        }else if (weight < height){
-            System.out.println("Weight is less than height");
-        }else {
-            System.out.println("Undefined");
+
+        switch (weight) {
+            case 10:
+                System.out.println("Your weight is 10%");
+                break;
+            case 20:
+                System.out.println("Your weight is 20%");
+                break;
+            case 80:
+                System.out.println("Your weight is 80%");
+                break;
+            default:
+                System.out.println("My default case");
         }
 
+
+
+
+        // if statement
+//        int weight, height;
+//        weight = 80;
+//        height = 180;
+//        if (weight == height){
+//            System.out.println("Weight is equal");
+//        }else if (weight > height){
+//            System.out.println("Weight is greater than height");
+//        }else if (weight < height){
+//            System.out.println("Weight is less than height");
+//        }else {
+//            System.out.println("Undefined");
+//        }
 
 
     }
