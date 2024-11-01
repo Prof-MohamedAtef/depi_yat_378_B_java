a7b374e2ab216a7f554e741b38f90ca7e554cd82
Mohamed Atef <proofesser@gmail.com>
2024-11-01 20:30:18 +0200

    Added int and String array

--- src/yatzaqziq/lecturethree/LectureThree.java
+++ src/yatzaqziq/lecturethree/LectureThree.java
@@ -3,25 +3,57 @@ package yatzaqziq.lecturethree;
 public class LectureThree {
     public static void main(String[] args) {
 
+        int x = 5;
+        int[] numsArray = new int[x]; // new array initialization
+        numsArray[0] = 10;
+        numsArray[1] = 500;
+        numsArray[2] = 15;
+        numsArray[3] = 200;
+        numsArray[4] = 25;
+
+        int[] numbers = {5, 2, 8, 100000};
+
+        String name = "Mohamed";
+        String[] namesArray = new String[6];
+        namesArray[0] = "Mohamed";
+        namesArray[1] = "Atef";
+        namesArray[2] = "Mahmoud";
+        namesArray[3] = "Sally";
+        namesArray[4] = "Ebrahim";
+        namesArray[5] = "Haneen";
+        namesArray[7] = "Ali";
+
+
+        String[] names = {"ahmed", "mohamed", "asdas"};
+
+
+
+
+
+
+
         // control statements
 //        switch statement
-        int weight, height;
-        weight = 10;
-        height = 180;
-
-        switch (weight) {
-            case 10:
-                System.out.println("Your weight is 10%");
-                break;
-            case 20:
-                System.out.println("Your weight is 20%");
-                break;
-            case 80:
-                System.out.println("Your weight is 80%");
-                break;
-            default:
-                System.out.println("My default case");
-        }
+//        int weight, height;
+//        weight = 10;
+//        height = 180;
+//
+//        switch (weight) {
+//            case 10:
+//                System.out.println("Your weight is 10%");
+//                break;
+//            case 20:
+//                System.out.println("Your weight is 20%");
+//                break;
+//            case 80:
+//                System.out.println("Your weight is 80%");
+//                break;
+//            default:
+//                System.out.println("My default case");
+//        }
+//
+//
+//
 
 
 
