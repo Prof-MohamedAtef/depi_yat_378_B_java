a86f193f0b7f74146668c36156cdeec4a3116e08
Mohamed Atef <proofesser@gmail.com>
2024-11-01 18:18:35 +0200

    if statement

---
+++ src/yatzaqziq/lecturethree/LectureThree.java
@@ -0,0 +1,21 @@
+package yatzaqziq.lecturethree;
+
+public class LectureThree {
+    public static void main(String[] args) {
+        int weight, height;
+        weight = 80;
+        height = 180;
+        if (weight == height){
+            System.out.println("Weight is equal");
+        }else if (weight > height){
+            System.out.println("Weight is greater than height");
+        }else if (weight < height){
+            System.out.println("Weight is less than height");
+        }else {
+            System.out.println("Undefined");
+        }
+
+
+
+    }
+}
