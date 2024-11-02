b7c567f894620b370298ae999d5e32f3901f87fb
Mohamed Atef <proofesser@gmail.com>
2024-11-02 19:16:51 +0200

    if statement revvision

--- src/ictgharbia/lecturethree/LectureThree.java
+++ src/ictgharbia/lecturethree/LectureThree.java
@@ -2,6 +2,17 @@ package ictgharbia.lecturethree;
 
 public class LectureThree {
     public static void main(String[] args) {
+        // Control Flow Statements
 
+
+
+
+//        // if Statement
+//        int width = 20, height = 20;
+//        if (width == height){
+//            System.out.println("The width and height are the same");
+//        }
+
+        // Con
     }
 }
