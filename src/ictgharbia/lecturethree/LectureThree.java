75270c140fb8aefdeaf711ed5d53d1e203b0e8fa
Mohamed Atef <proofesser@gmail.com>
2024-11-02 20:29:17 +0200

    Control Flow Statements | Switch Statement ++

--- src/ictgharbia/lecturethree/LectureThree.java
+++ src/ictgharbia/lecturethree/LectureThree.java
@@ -7,21 +7,21 @@ public class LectureThree {
 
         // switch statement
 
-        int width = 100;
-        switch (width){
-            case 20:
-                System.out.println("Width is 20");
-                break;
-            case 50:
-                System.out.println("Width is 50");
-                break;
-            case 80:
-                System.out.println("Width is 80");
-                break;
-            default:
-                System.out.println("Invalid data");
-                break;
-        }
+//        int width = 100;
+//        switch (width){
+//            case 20:
+//                System.out.println("Width is 20");
+//                break;
+//            case 50:
+//                System.out.println("Width is 50");
+//                break;
+//            case 80:
+//                System.out.println("Width is 80");
+//                break;
+//            default:
+//                System.out.println("Invalid data");
+//                break;
+//        }
 
 //        // if Statement
 //        int width = 20, height = 20;
