3f4090df856eabd049065447fad2bffa150db337
Dr.Mohamed-Atef <proofesser@gmail.com>
2024-11-08 10:25:45 -0800

    Added lecture-task.txt

--- src/yatzaqziq/lecturefive/Main.java
+++ src/yatzaqziq/lecturefive/Main.java
@@ -8,27 +8,28 @@ public class Main {
 
 //        Vehicle vehicle = new Vehicle(0, "asdasd");
 //        Vehicle vehicle1 = new Vehicle("Mercedes", "C300", "Car");
-        Vehicle vehicle = new Vehicle();
+//        Vehicle vehicle = new Vehicle();
+//
+//        new Vehicle("Mercedes", "C300", "Car");
+//
+//        vehicle.setData("Mercedes", "C300", "#EEEEEE", "Car", true, 4);
+//        System.out.println("Hello");
+//
+//        System.out.println(vehicle.returnData());
+//
+//        vehicle.returnData(0, "");
+//
+//        vehicle.returnData("name", 5);
 
-        new Vehicle("Mercedes", "C300", "Car");
+//        Scanner reader = new Scanner(System.in);
+//
+//        System.out.println("Enter Input");
+//        String data = reader.nextLine();
+//
+//        System.out.println("Input=" + data + "\n" + "Input length: " + vehicle.printLength(data));
+//
+//        String fullName = vehicle.fullNameToLower("MOHAMED", "ATEF");
+//        System.out.println(fullName);
 
-        vehicle.setData("Mercedes", "C300", "#EEEEEE", "Car", true, 4);
-        System.out.println("Hello");
-
-        System.out.println(vehicle.returnData());
-
-        vehicle.returnData(0, "");
-
-        vehicle.returnData("name", 5);
-
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Enter Input");
-        String data = reader.nextLine();
-
-        System.out.println("Input=" + data + "\n" + "Input length: " + vehicle.printLength(data));
-
-        String fullName = vehicle.fullNameToLower("MOHAMED", "ATEF");
-        System.out.println(fullName);
     }
 }
